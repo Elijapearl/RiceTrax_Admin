0e32aca6d32f428b7af07c666f8c1d303469d5bb
Elijapearl <elipearlparafina@gmail.com>
2025-07-02 08:56:24 +0800

    have table in Rice Inventory Stock

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'Dashboard.dart';
+import 'package:rice_trax/Dashboard.dart';
+import 'package:rice_trax/RiceStock.dart';
 
 void main() {
   runApp(RiceTraxApp());
@@ -9,7 +10,7 @@ class RiceTraxApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Dashboard(),
+      home: RiceStock(),
       debugShowCheckedModeBanner: false,
     );
   }
